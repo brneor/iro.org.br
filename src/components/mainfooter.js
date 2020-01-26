1591f570a098016b5da5480968b811636ec90e50
framtava <framtava@users.noreply.github.com>
2020-01-25 23:20:15 -0300

    Mudanças rodapé

--- src/components/mainfooter.js
+++ src/components/mainfooter.js
@@ -5,27 +5,25 @@ import { Container, Col, Row } from "reactstrap"
 const CustomFooter = (props) => {
   return (
     <div>
-      <footer className="bg-dark text-light py-5 mt-5 footer text_font_default">
+      <footer className="bg-dark text-light mt-5 footer text_font_default">
         <Container>
           <Row>
             <Col>
-              <h1>Encontre-nos</h1>
+              <p><span class="smallcaps">Encontre-nos</span></p>
               <p className="links">
-                <a href="mailto:contato@iro.org.br" target="blank" title="Email"><img src="email-icon.svg" /></a>
-                <a href="https://facebook.com/sola.scriptura500" target="blank" title="Facebook"><img src="facebook-icon.svg" /></a>
-                <a href="https://youtube.com/" target="blank" title="You Tube"><img src="youtube-icon.svg" /></a>
+                <a href="mailto:contato@iro.org.br" target="blank" title="Email"><img src="/email-icon.svg" /></a>
+                <a href="https://facebook.com/sola.scriptura500" target="blank" title="Facebook"><img src="/facebook-icon.svg" /></a>
+                <a href="https://youtube.com/" target="blank" title="You Tube"><img src="/youtube-icon.svg" /></a>
               </p>
             </Col>
             <Col>
-              <h1>Conheça a Igreja</h1>
+              <p><span class="smallcaps">Conheça a Igreja</span></p>
               <p className="info text_font_default">
-                <span className="bold">Endereço</span>
                 <span>Av. Sete, 1140</span>
                 <span>Juiz de Fora MG, CEP: 36063-000</span>
               </p>
 
               <p className="info text_font_default">
-                <span className="bold">Horário dos cultos</span>
                 <span>Terça: 19h30</span>
                 <span>Sexta: 19h30</span>
                 <span>Domingo: 19h</span>
@@ -33,7 +31,7 @@ const CustomFooter = (props) => {
             </Col>
           </Row>
           <Row>
-            <Col><p>© 2020 Igreja Reformada Ortodoxa</p></Col>
+            <Col><p class="copyright">© 2020 Igreja Reformada Ortodoxa</p></Col>
           </Row>
         </Container>
       </footer>
